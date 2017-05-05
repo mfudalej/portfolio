8f9ed8dbd2611c0aeaa694f31453edee568dc34a
Mateusz Fudalej <mateusz.fudalej@gmail.com>
2017-05-05 02:49:01 +0200

    typed.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,14 @@
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+//function topFunction() {
+//    document.body.scrollTop = 0;
+//    document.documentElement.scrollTop = 0;
+//}
+
+document.addEventListener("DOMContentLoaded", function(){
+		Typed.new(".typed", {
+			strings: ["żyjącym na codzień w Warszawie.", "szukającym pierwszej pracy w IT." ],
+			backDelay: 1200,
+	        loop: true,
+	        typeSpeed: 30
+		});
+	});
+
