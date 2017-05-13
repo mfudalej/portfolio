3a6b3c9dd979d0647400397c4917de0ef0280068
Mateusz Fudalej <mateusz.fudalej@gmail.com>
2017-05-13 22:30:16 +0200

    fix nav

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,12 @@
-//function topFunction() {
-//    document.body.scrollTop = 0;
-//    document.documentElement.scrollTop = 0;
-//}
-
 document.addEventListener("DOMContentLoaded", function(){
-		Typed.new(".typed", {
-			strings: ["żyjącym na codzień w Warszawie.", "szukającym pierwszej pracy w IT." ],
-			backDelay: 1200,
-	        loop: true,
-	        typeSpeed: 30
-		});
-	});
+    Typed.new(".typed", {
+        strings: ["żyjącym na codzień w Warszawie.", "szukającym pierwszej pracy w IT." ],
+        backDelay: 1200,
+        loop: true,
+        typeSpeed: 30
+    });
+    
+});
+
+
 
